8735b5be00ba5bb621a2ed5ada81171fabd7002e
Romildo Jozue Paiter <romildopaiter@gmail.com>
2017-03-09 21:20:12 -0400

    Commit Initial

---
+++ API/src/API/Controllers/IdentityController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+                
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [AcceptVerbs("POST", "PUT")]
+        public IActionResult Add(string title)
+        {
+            Console.Write("Inseri titulo");
+            return Ok();
+        }
+
+    }
+}
---
+++ IdentityServer4.SSO/src/IdentityServer4.SSO/Config.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Security.Claims;
+
+namespace IdentityServer4.SSO
+{
+    internal class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource("api", "ResourceWebApi"),
+                new ApiResource("api2", "ResourceWebApi")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                // OpenID Connect hybrid flow and client credentials client (MVC)
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = { "http://localhost:5001/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5001" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api"
+                    },
+                    AllowOfflineAccess = true
+                },
+
+                new Client
+                {
+                    ClientId = "mvc2",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api"
+                    },
+                    AllowOfflineAccess = true
+                },
+
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =           { "http://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5003" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api"
+                    }
+                },
+
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js2",
+                    ClientName = "JavaScript Client 2",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =           { "http://localhost:5004/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5004/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5004" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api"
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUser()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "gpaiter",
+                    Password = "123",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Guilherme Cervantes Paiter"),
+                        new Claim("profile", "ADMIN"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "rpaiter",
+                    Password = "123",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Romildo Jozué Paiter"),
+                        new Claim("website", "http://www.eunaosoubobo.com"),
+                        new Claim("permission", ""),
+                    }
+                }
+            };
+        }
+    }
+}
